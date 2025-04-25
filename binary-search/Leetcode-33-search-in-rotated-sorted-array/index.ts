2beeab1c7e301e5786d75521d8c306a69688a07c
AndreGeng <andregeng2011@gmail.com>
2025-04-25 22:03:47 +0800

    feat: 33 Search in Rotated Sorted Array

---
+++ binary-search/Leetcode-33-search-in-rotated-sorted-array/index.ts
@@ -0,0 +1,21 @@
+function search(nums: number[], target: number): number {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left < right) {
+    const mid = (left + right) >> 1;
+    if (nums[0] < nums[mid]) {
+      if (target >= nums[0] && target <= nums[mid]) {
+        right = mid;
+      } else {
+        left = mid + 1;
+      }
+    } else {
+      if (target >= nums[mid] && target <= nums[right]) {
+        left = mid;
+      } else {
+        right = mid - 1;
+      }
+    }
+  }
+  return nums[left] === target ? left : -1;
+}
