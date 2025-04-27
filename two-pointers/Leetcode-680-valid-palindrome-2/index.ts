7a312906b7db81219cdbc7752b7f8764eed5cd27
AndreGeng <andregeng2011@gmail.com>
2025-04-27 13:28:15 +0800

    feat: 680 valid palindrome 2

---
+++ two-pointers/Leetcode-680-valid-palindrome-2/index.ts
@@ -0,0 +1,26 @@
+function isPalindrome(s: string, i: number = 0, j: number = s.length - 1) {
+  let left = i;
+  let right = j;
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
+}
+function validPalindrome(s: string): boolean {
+  let left = 0;
+  let right = s.length - 1;
+  while (left < right) {
+    if (s[left] !== s[right]) {
+      return (
+        isPalindrome(s, left, right - 1) || isPalindrome(s, left + 1, right)
+      );
+    }
+    left++;
+    right--;
+  }
+  return true;
+}
