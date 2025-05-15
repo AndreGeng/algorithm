a971b57ca3a998fc00411006eccb8183631848ff
AndreGeng <andregeng2011@gmail.com>
2025-05-15 09:59:06 +0800

    feat: Leetcode 21 Merge Two Sorted Lists

---
+++ linked-list/Leetcode-21-merge-two-sorted-list/index.ts
@@ -0,0 +1,30 @@
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function mergeTwoLists(
+  list1: ListNode | null,
+  list2: ListNode | null,
+): ListNode | null {
+  let current1 = list1;
+  let current2 = list2;
+  const dummy = new ListNode(-1);
+  let tail = dummy;
+  while (current1 && current2) {
+    if (current1.val < current2.val) {
+      tail.next = current1;
+      current1 = current1.next;
+    } else {
+      tail.next = current2;
+      current2 = current2.next;
+    }
+    tail = tail.next;
+  }
+  tail.next = current1 || current2;
+  return dummy.next;
+}
